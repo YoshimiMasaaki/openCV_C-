aea513b0b93f225c94a419ac8edff636a263398a
YoshimiMasaaki <yoshimi@prosys.co.jp>
2018-06-11 14:39:34 +0900

    C# to openCV project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        ClassLibrary1.Class1 clsTest;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clsTest = new ClassLibrary1.Class1();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = clsTest.TestMethod();
+        }
+    }
+}
